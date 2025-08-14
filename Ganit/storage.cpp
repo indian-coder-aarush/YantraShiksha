0a60f1c10e81e73a17165a2284c4b156fe89fb70
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-08-14 20:45:28 +0530

    fixed evrything finally

--- Ganit/storage.cpp
+++ Ganit/storage.cpp
@@ -298,10 +298,10 @@ storage storage::slice(std::vector<std::vector<int>>& slice){
    for(int i=0;i<slice.size();i++){
        if(slice[i].size() != 1){
        if(slice[i][2] < 0){
-          index_offset += stride[i]*(slice[i][1] > 0 ? slice[i][1]:shape[i]+slice[i][1]);
+          index_offset += stride[i]*(slice[i][1] >= 0 ? slice[i][1]:shape[i]+slice[i][1]);
        }
        else{
-          index_offset += stride[i]*(slice[i][0] > 0 ? slice[i][0]:shape[i]+slice[i][0]);
+          index_offset += stride[i]*(slice[i][0] >= 0 ? slice[i][0]:shape[i]+slice[i][0]);
        }
        int len = 0;
        if ((slice[i][2] > 0 && slice[i][0] < slice[i][1]) || (slice[i][2] < 0 && slice[i][0] > slice[i][1])) {
@@ -309,10 +309,6 @@ storage storage::slice(std::vector<std::vector<int>>& slice){
        }
        new_shape.push_back(len);
        new_stride.push_back(stride[i]*slice[i][2]);
-       }
-       else{
-           index_offset += ((shape.size() != i+1)&&(slice[i+1][2] < 0) ? stride[i+1]:stride[i])*(((shape.size() != i)&&
-           (slice[i+1][2] < 0))?(shape[i+1] - 1):(slice[i][0] >= 0 ? slice[i][0]:shape[i]+slice[i][0]));
        }
    }
    if(new_shape.size()!=shape.size()){
