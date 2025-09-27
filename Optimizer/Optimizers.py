0be78ed3600d6681e2f38b74ae117a0907714438
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-27 14:25:07 +0530

    made a condition

--- Optimizer/Optimizers.py
+++ Optimizer/Optimizers.py
@@ -8,4 +8,5 @@ class GradientDescent:
 
     def step(self):
         for param_key in self.params:
-            self.params[param_key] -= self.learning_rate * self.params[param_key].grad
+            if self.params[param_key].grad is not None:
+                self.params[param_key] -= self.learning_rate * self.params[param_key].grad
