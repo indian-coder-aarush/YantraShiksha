a11a3fb9938fbee0293876e549aaa80059414464
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-27 14:19:17 +0530

    made an optimizer

--- Optimizer/Optimizers.py
+++ Optimizer/Optimizers.py
@@ -5,3 +5,7 @@ class GradientDescent:
     def __init__(self,learning_rate,params):
         self.learning_rate = learning_rate
         self.params = params
+
+    def step(self):
+        for param_key in self.params:
+            self.params[param_key] -= self.learning_rate * self.params[param_key].grad
